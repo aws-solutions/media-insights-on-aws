fcba4f2d450affaefcf6502a7846610e57c175c6
ianwow <ianwow@amazon.com>
2019-11-06 14:42:59 -0800

    check that face attributes exist before trying to read their value

--- webapp/src/components/FaceDetection.vue
+++ webapp/src/components/FaceDetection.vue
@@ -184,50 +184,64 @@
             for (var i = 0, len = data.length; i < len; i++) {
               let item = data[i]._source
               // need to also loop thru emotions
-              if (item.Emotions === true) {
+              if ("Emotions" in item) {
                 for (var emotion = 0, emotionsLen = item.Emotions.length; emotion < emotionsLen; emotion++) {
                   if (item.Emotions[emotion].Confidence >= this.Confidence) {
                     es_data.push({"Name": item.Emotions[emotion].Type, "Timestamp": item.Timestamp})
                   }
                 }
               }
-              if (item.Beard.Value === true) {
+              if ("Beard" in item) {
                 if (item.Beard.Confidence > this.Confidence) {
                   es_data.push({"Name": "Beard", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.Eyeglasses.Value === true) {
+              if ("Eyeglasses" in item) {
                 if (item.Eyeglasses.Confidence > this.Confidence) {
                   es_data.push({"Name": "Eyeglasses", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.EyesOpen.Value === true) {
+              if ("EyesOpen" in item) {
                 if (item.EyesOpen.Confidence > this.Confidence) {
                   es_data.push({"Name": "EyesOpen", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.MouthOpen.Value === true) {
+              if ("MouthOpen" in item) {
                 if (item.MouthOpen.Confidence > this.Confidence) {
                   es_data.push({"Name": "MouthOpen", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.Mustache.Value === true) {
+              if ("Mustache" in item) {
                 if (item.Mustache.Confidence > this.Confidence) {
                   es_data.push({"Name": "Mustache", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.Smile.Value === true) {
+              if ("Smile" in item) {
                 if (item.Smile.Confidence > this.Confidence) {
                   es_data.push({"Name": "Smile", "Timestamp": item.Timestamp})
                 }
               }
-              if (item.Sunglasses.Value === true) {
+              if ("Sunglasses" in item) {
                 if (item.Sunglasses.Confidence > this.Confidence) {
                   es_data.push({"Name": "Sunglasses", "Timestamp": item.Timestamp})
                 }
               }
-              es_data.push({"Name": item.Gender.Value, "Timestamp": item.Timestamp})
-              es_data.push({"Name": "Face", "Timestamp": item.Timestamp, "Confidence": item.Confidence, "BoundingBox": {"Width": item.BoundingBox.Width, "Height": item.BoundingBox.Height, "Left": item.BoundingBox.Left, "Top": item.BoundingBox.Top}})
+              if ("Gender" in item) {
+                es_data.push({"Name": item.Gender.Value, "Timestamp": item.Timestamp})
+              }
+              if ("BoundingBox" in item) {
+                es_data.push({
+                  "Name": "Face",
+                  "Timestamp": item.Timestamp,
+                  "Confidence": item.Confidence,
+                  "BoundingBox": {
+                    "Width": item.BoundingBox.Width,
+                    "Height": item.BoundingBox.Height,
+                    "Left": item.BoundingBox.Left,
+                    "Top": item.BoundingBox.Top
+                  }
+                })
+              }
             }
             this.elasticsearch_data = JSON.parse(JSON.stringify(es_data))
             this.isBusy = false

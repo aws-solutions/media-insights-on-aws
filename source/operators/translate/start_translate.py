f4862b5d4f568f56ff0423a1fb600cee89e8f4b4
ianwow <ianwow@amazon.com>
2019-12-04 14:22:38 -0800

    Split large transcripts along sentence boundaries prior to sending transcript chunks to Translate

--- source/operators/translate/start_translate.py
+++ source/operators/translate/start_translate.py
@@ -6,6 +6,7 @@ import os
 import json
 from botocore import config
 import math
+import nltk.data
 
 from MediaInsightsEngineLambdaHelper import DataPlane
 from MediaInsightsEngineLambdaHelper import MediaInsightsOperationHelper
@@ -60,49 +61,85 @@ def lambda_handler(event, context):
         operator_object.add_workflow_metadata(TranslateError="Unable to read transcription from S3: {e}".format(e=str(e)))
         raise MasExecutionError(operator_object.return_output_object())
 
+    # Tell the NLTK data loader to look for files in /tmp/
+    nltk.data.path.append("/tmp/")
+    # Download NLTK tokenizers to /tmp/
+    # We use /tmp because that's where AWS Lambda provides write access to the local file system.
+    nltk.download('punkt', download_dir='/tmp/')
+    # Create language tokenizer according to user-specified source language.
+    # Default to English.
+    if source_lang == 'fr':
+        print("Using French dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/french.pickle')
+    elif source_lang == 'de':
+        print("Using German dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/german.pickle')
+    elif source_lang == 're':
+        print("Using Russian dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/russian.pickle')
+    elif source_lang == 'it':
+        print("Using Italian dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/italian.pickle')
+    elif source_lang == 'pt':
+        print("Using Portuguese dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle')
+    elif source_lang == 'es':
+        print("Using Spanish dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
+    else:
+        print("Using English dictionary to find sentence boundaries.")
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-    # TODO: This transcript splitter will fragment words and sentences, which will lead to
-    #  less accurate translations. The python nltk package could be used to avoid this
-    #  problem, like this:
-    #  Tokenize transcript into sentences with nltk
-    #  for each sentence
-    #    if a sentence is > 5000 length, print error and bail.
-    #    append sentences but don't exceed 5000 characters
-    #    process translate on that appended chunk
-
-    try:
-        # Translate can handle 5000 unicode characters but we'll process only 4000 at a time
-        # in case the input text is ascii encoded.
-        # (A unicode character can be more than one ascii character).
-        translated_text = ''
-        print("Input text length: " + str(len(transcript)))
-        for i in range(math.ceil(len(transcript)/4000)):
-            transcript_chunk = transcript[i*4000:i*4000+3999]
-            print("Processing transcript characters " + str(i*4000) + " through " + str(i*4000+3999))
-            translation_chunk = translate_client.translate_text(Text=transcript_chunk,SourceLanguageCode=source_lang,TargetLanguageCode=target_lang)
+    # Split input text into a list of sentences
+    sentences = tokenizer.tokenize(transcript)
+    print("Input text length: " + str(len(transcript)))
+    print("Number of sentences: " + str(len(sentences)))
+    translated_text = ''
+    transcript_chunk = ''
+    for sentence in sentences:
+        # Translate can handle 5000 unicode characters but we'll process no more than 4000
+        # just to be on the safe side.
+        if (len(sentence) + len(transcript_chunk) < 4000):
+            transcript_chunk = transcript_chunk + ' ' + sentence
+        else:
+            try:
+                print("Translation input text length: " + str(len(transcript_chunk)))
+                translation_chunk = translate_client.translate_text(Text=transcript_chunk,SourceLanguageCode=source_lang,TargetLanguageCode=target_lang)
+                print("Translation output text length: " + str(len(translation_chunk)))
+            except Exception as e:
+                operator_object.update_workflow_status("Error")
+                operator_object.add_workflow_metadata(TranslateError="Unable to get response from translate: {e}".format(e=str(e)))
+                raise MasExecutionError(operator_object.return_output_object())
             translated_text = translated_text + ' ' + translation_chunk["TranslatedText"]
+            transcript_chunk = sentence
+    print("Translating the final chunk of input text...")
+    try:
+        print("Translation input text length: " + str(len(transcript_chunk)))
+        translation_chunk = translate_client.translate_text(Text=transcript_chunk,SourceLanguageCode=source_lang,TargetLanguageCode=target_lang)
+        print("Translation output text length: " + str(len(translation_chunk)))
     except Exception as e:
         operator_object.update_workflow_status("Error")
         operator_object.add_workflow_metadata(TranslateError="Unable to get response from translate: {e}".format(e=str(e)))
         raise MasExecutionError(operator_object.return_output_object())
+    translated_text = translated_text + ' ' + translation_chunk["TranslatedText"]
+    # Put final result into a JSON object because the MIE dataplane requires it to be so.
+    translation_result = {}
+    translation_result["TranslatedText"] = translated_text
+    print("Final translation text length: " + str(len(translated_text)))
+    dataplane = DataPlane()
+    metadata_upload = dataplane.store_asset_metadata(asset_id, operator_object.name, workflow_id, translation_result)
+    if "Status" not in metadata_upload:
+        operator_object.add_workflow_metadata(
+            TranslateError="Unable to upload metadata for asset: {asset}".format(asset=asset_id))
+        operator_object.update_workflow_status("Error")
+        raise MasExecutionError(operator_object.return_output_object())
     else:
-        print("Translated text length: " + str(len(transcript)))
-        dataplane = DataPlane()
-        translation_chunk["TranslatedText"] = translated_text
-        translation = translation_chunk
-        metadata_upload = dataplane.store_asset_metadata(asset_id, operator_object.name, workflow_id, translation)
-        if "Status" not in metadata_upload:
+        if metadata_upload['Status'] == 'Success':
+            operator_object.add_media_object('Text', metadata_upload['Bucket'], metadata_upload['Key'])
+            operator_object.update_workflow_status("Complete")
+            return operator_object.return_output_object()
+        else:
             operator_object.add_workflow_metadata(
                 TranslateError="Unable to upload metadata for asset: {asset}".format(asset=asset_id))
             operator_object.update_workflow_status("Error")
             raise MasExecutionError(operator_object.return_output_object())
-        else:
-            if metadata_upload['Status'] == 'Success':
-                operator_object.add_media_object('Text', metadata_upload['Bucket'], metadata_upload['Key'])
-                operator_object.update_workflow_status("Complete")
-                return operator_object.return_output_object()
-            else:
-                operator_object.add_workflow_metadata(
-                    TranslateError="Unable to upload metadata for asset: {asset}".format(asset=asset_id))
-                operator_object.update_workflow_status("Error")
-                raise MasExecutionError(operator_object.return_output_object())

119bde3020d3ee2f014b037beceddb574df2c696
ianwow <ianwow@amazon.com>
2019-12-12 12:03:46 -0800

    work around bug in amplify-authenticator that breaks autofill in the 1password browser plugin

--- webapp/src/views/Login.vue
+++ webapp/src/views/Login.vue
@@ -5,6 +5,31 @@
 </template>
 
 <script>
+//-----------------------------------------------------------//
+// Hack to work around a bug in amplify-authenticator, which makes it so it
+// doesn't notice if a username is autofilled rather than being typed in.
+// This work around enables the 1password browser plugin to autofill
+// username and password.
+//
+// See https://github.com/aws-amplify/amplify-js/issues/4374
+//
+// This hack can be removed once the issue is resolved.
+function patchSignIn () {
+  // monkey-patch the UsernameField component, and add a watcher to make it properly emit a changed
+  // event when the username field changes
+  let usernameComponent = Vue.component('amplify-username-field')
+  let watches = usernameComponent.options.watch = usernameComponent.options.watch || {}
+  watches.username = function () {
+    this.usernameChanged()
+  }
+  watches.email = function () {
+    this.emailChanged()
+  }
+}
+import Vue from 'vue'
+patchSignIn()
+//-----------------------------------------------------------//
+
 import { AmplifyEventBus } from "aws-amplify-vue";
 export default {
   name: "Login",

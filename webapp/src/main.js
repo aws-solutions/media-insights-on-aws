5ef34bd3ad30149f20d0c372d7809dd68ee1fdf5
ianwow <ianwow@amazon.com>
2020-01-08 11:06:38 -0800

    get runtime config with ajax call to /runtimeConfig.json and distribute that config to all Vue components

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -8,30 +8,57 @@ import 'dropzone/dist/min/dropzone.min.css'
 import App from './App.vue'
 import store from './store'
 import router from './router.js'
-
 import Amplify, * as AmplifyModules from "aws-amplify";
 import { AmplifyPlugin } from "aws-amplify-vue";
-import awsconfig from "@/aws-exports";
 
-Amplify.configure(awsconfig);
+const getRuntimeConfig = async () => {
+  const runtimeConfig = await fetch('/runtimeConfig.json');
+  return await runtimeConfig.json()
+}
 
-Vue.config.productionTip = false
-
-var marked = require('marked');
-
-Vue.mixin({
-  methods: {
-    marked: function (input) {
-      return marked(input);
+getRuntimeConfig().then(function(json) {
+  const awsconfig = {
+    Auth: {
+      region: json.AWS_REGION,
+      userPoolId: json.USER_POOL_ID,
+      userPoolWebClientId: json.USER_POOL_CLIENT_ID,
+      identityPoolId: json.IDENTITY_POOL_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: "mieElasticsearch",
+          endpoint: json.ELASTICSEARCH_ENDPOINT,
+          service: "es",
+          region: json.AWS_REGION
+        }
+      ]
     }
-  }
+  };
+  console.log("Runtime config: " + JSON.stringify(json))
+  Amplify.configure(awsconfig);
+  Vue.config.productionTip = false
+  var marked = require('marked');
+  Vue.mixin({
+    methods: {
+      marked: function (input) {
+        return marked(input);
+      }
+    },
+    data() {
+      return {
+        // Distribute the runtime config into every Vue component
+        runtimeConfig: json
+      }
+    },
+  });
+
+  Vue.use(AmplifyPlugin, AmplifyModules);
+  Vue.use(BootstrapVue)
+
+  new Vue({
+    router,
+    store,
+    render: h => h(App),
+  }).$mount('#app')
 });
-
-Vue.use(AmplifyPlugin, AmplifyModules);
-Vue.use(BootstrapVue)
-
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')

fd20ba4da3c89d33675792b3009684bf5cfc5b0b
ianwow <ianwow@amazon.com>
2020-02-13 10:03:24 -0800

    Apply bash syntax optimizations

--- deployment/lambda_layer_factory/build-lambda-layer.sh
+++ deployment/lambda_layer_factory/build-lambda-layer.sh
@@ -21,7 +21,7 @@
 
 
 # Check to see if input has been provided:
-if [ -z "$1" ] || ([ ! -z "$2" ] && [ -z "$3" ]); then
+if [ -z "$1" ] || ([ -n "$2" ] && [ -z "$3" ]); then
     echo "Please provide a fully qualified s3 bucket for the lambda layer code to reside and the region of the deploy."
     echo "USAGE: ./build-lambda-layer.sh <requirements.txt> [s3 bucket path] [aws region]"
     echo "To build but not publish the Lambda layer zip files:"
@@ -31,10 +31,9 @@ if [ -z "$1" ] || ([ ! -z "$2" ] && [ -z "$3" ]); then
     exit 1
 fi
 
-REQUIREMENTS_FILE=$1
-S3_BUCKET=$(echo $2 | cut -f 3 -d "/")
-S3_FQDN=$2
-REGION=$3
+REQUIREMENTS_FILE="$1"
+S3_BUCKET=$(echo "$2" | cut -f 3 -d "/")
+S3_FQDN="$2"
 
 # Check to see if requirements.txt file exists
 
@@ -46,15 +45,23 @@ fi
 # Check to see if AWS CLI and Docker are installed
 docker --version
 if [ $? -ne 0 ]; then
-    echo "ERROR: install and start Docker before running this script"
-    exit 1
+  echo "ERROR: install Docker before running this script"
+  exit 1
+else
+  pgrep -f docker
+  if [ $? -ne 0 ]; then
+      echo "ERROR: start Docker before running this script"
+      exit 1
+  fi
 fi
 
 echo "------------------------------------------------------------------------------"
 echo "Building Lambda Layer zip file"
 echo "------------------------------------------------------------------------------"
-docker build --tag=lambda_layer_factory:latest .
-docker run --rm -it -v $(pwd):/packages lambda_layer_factory
+docker build --tag=lambda_layer_factory:latest . 2>&1 > /dev/null
+if [ $? -eq 0 ]; then
+  docker run --rm -it -v "$PWD":/packages lambda_layer_factory
+fi
 if [[ ! -f ./lambda_layer-python3.6.zip ]] || [[ ! -f ./lambda_layer-python3.7.zip ]] || [[ ! -f ./lambda_layer-python3.8.zip ]]; then
     echo "ERROR: Failed to build lambda layer zip file."
     exit 1
@@ -68,12 +75,12 @@ unzip -q -d lambda_layer-python-3.7 ./lambda_layer-python3.7.zip
 unzip -q -d lambda_layer-python-3.8 ./lambda_layer-python3.8.zip
 ZIPPED_LIMIT=50
 UNZIPPED_LIMIT=250
-UNZIPPED_SIZE_36=`du -sm ./lambda_layer-python-3.6/ | cut -f 1`
-ZIPPED_SIZE_36=`du -sm ./lambda_layer-python3.6.zip | cut -f 1`
-UNZIPPED_SIZE_37=`du -sm ./lambda_layer-python-3.7/ | cut -f 1`
-ZIPPED_SIZE_37=`du -sm ./lambda_layer-python3.7.zip | cut -f 1`
-UNZIPPED_SIZE_38=`du -sm ./lambda_layer-python-3.8/ | cut -f 1`
-ZIPPED_SIZE_38=`du -sm ./lambda_layer-python3.8.zip | cut -f 1`
+UNZIPPED_SIZE_36=$(du -sm ./lambda_layer-python-3.6/ | cut -f 1)
+ZIPPED_SIZE_36=$(du -sm ./lambda_layer-python3.6.zip | cut -f 1)
+UNZIPPED_SIZE_37=$(du -sm ./lambda_layer-python-3.7/ | cut -f 1)
+ZIPPED_SIZE_37=$(du -sm ./lambda_layer-python3.7.zip | cut -f 1)
+UNZIPPED_SIZE_38=$(du -sm ./lambda_layer-python-3.8/ | cut -f 1)
+ZIPPED_SIZE_38=$(du -sm ./lambda_layer-python3.8.zip | cut -f 1)
 if (( $UNZIPPED_SIZE_36 > $UNZIPPED_LIMIT || $ZIPPED_SIZE_36 > $ZIPPED_LIMIT || $UNZIPPED_SIZE_37 > $UNZIPPED_LIMIT || $ZIPPED_SIZE_37 > $ZIPPED_LIMIT || $UNZIPPED_SIZE_38 > $UNZIPPED_LIMIT || $ZIPPED_SIZE_38 > $ZIPPED_LIMIT)); then
 	echo "ERROR: Deployment package exceeds AWS Lambda layer size limits.";
 	rm -f ./lambda_layer-python3.6.zip
@@ -86,8 +93,8 @@ if (( $UNZIPPED_SIZE_36 > $UNZIPPED_LIMIT || $ZIPPED_SIZE_36 > $ZIPPED_LIMIT || 
 fi
 echo "Lambda layers have been saved to ./lambda_layer-python3.6.zip, ./lambda_layer-python3.7.zip, and ./lambda_layer-python3.8.zip."
 
-if [ ! -z $S3_FQDN ]; then
-    which aws > /dev/null
+if [ -n "$S3_FQDN" ]; then
+    command -v aws > /dev/null
     if [ $? -ne 0 ]; then
         echo "ERROR: install the AWS CLI before running this script"
         exit 1
@@ -96,8 +103,8 @@ if [ ! -z $S3_FQDN ]; then
     echo "------------------------------------------------------------------------------"
     echo "Validating access to S3"
     echo "------------------------------------------------------------------------------"
-    aws s3 mb $S3_BUCKET 2> /dev/null
-    aws s3 ls $S3_BUCKET > /dev/null
+    aws s3 mb "$S3_BUCKET" 2> /dev/null
+    aws s3 ls "$S3_BUCKET" > /dev/null
     if [ $? -ne 0 ]; then
         echo "ERROR: Failed accessing $S3_FQDN"
     fi
@@ -109,18 +116,18 @@ if [ ! -z $S3_FQDN ]; then
     LAYER_NAME_37=lambda_layer-python37
     LAYER_NAME_38=lambda_layer-python38
     # create temp working dir for zip files
-    aws s3 mb s3://$LAMBDA_LAYERS_BUCKET > /dev/null
+    aws s3 mb s3://"$LAMBDA_LAYERS_BUCKET" > /dev/null
     # Warn user if layer already exists
-    aws lambda list-layer-versions --layer-name $LAYER_NAME_36 | grep -q "\"LayerVersions\": \[" 
+    aws lambda list-layer-versions --layer-name $LAYER_NAME_36 | grep -q "\"LayerVersions\": \["
     if [ $? -eq 0 ]; then
         echo "WARNING: AWS Layer with name $LAYER_NAME_36 already exists."
         read -r -p "Are you sure you want to overwrite $LAYER_NAME_36? [y/N] " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
         then
-            aws s3 cp lambda_layer-python3.6.zip s3://$LAMBDA_LAYERS_BUCKET
-            aws lambda publish-layer-version --layer-name $LAYER_NAME_36 --content S3Bucket=$LAMBDA_LAYERS_BUCKET,S3Key=lambda_layer-python3.6.zip --compatible-runtimes python3.6
-            aws s3 rm s3://$LAMBDA_LAYERS_BUCKET/lambda_layer-python3.6.zip
-            arn36=$(aws lambda list-layer-versions --layer-name lambda_layer-python36 --output text --query 'LayerVersions[0].LayerVersionArn')            
+            aws s3 cp lambda_layer-python3.6.zip s3://"$LAMBDA_LAYERS_BUCKET"
+            aws lambda publish-layer-version --layer-name $LAYER_NAME_36 --content S3Bucket="$LAMBDA_LAYERS_BUCKET",S3Key=lambda_layer-python3.6.zip --compatible-runtimes python3.6
+            aws s3 rm s3://"$LAMBDA_LAYERS_BUCKET"/lambda_layer-python3.6.zip
+            arn36=$(aws lambda list-layer-versions --layer-name lambda_layer-python36 --output text --query 'LayerVersions[0].LayerVersionArn')
         fi
     fi
     aws lambda list-layer-versions --layer-name $LAYER_NAME_37 | grep "\"LayerVersions\": \["
@@ -129,9 +136,9 @@ if [ ! -z $S3_FQDN ]; then
         read -r -p "Are you sure you want to overwrite $LAYER_NAME_37? [y/N] " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
         then
-            aws s3 cp lambda_layer-python3.7.zip s3://$LAMBDA_LAYERS_BUCKET
-            aws lambda publish-layer-version --layer-name $LAYER_NAME_37 --content S3Bucket=$LAMBDA_LAYERS_BUCKET,S3Key=lambda_layer-python3.7.zip --compatible-runtimes python3.7
-            aws s3 rm s3://$LAMBDA_LAYERS_BUCKET/lambda_layer-python3.7.zip
+            aws s3 cp lambda_layer-python3.7.zip s3://"$LAMBDA_LAYERS_BUCKET"
+            aws lambda publish-layer-version --layer-name $LAYER_NAME_37 --content S3Bucket="$LAMBDA_LAYERS_BUCKET",S3Key=lambda_layer-python3.7.zip --compatible-runtimes python3.7
+            aws s3 rm s3://"$LAMBDA_LAYERS_BUCKET"/lambda_layer-python3.7.zip
             arn37=$(aws lambda list-layer-versions --layer-name lambda_layer-python37 --output text --query 'LayerVersions[0].LayerVersionArn')
         fi
     fi
@@ -141,18 +148,18 @@ if [ ! -z $S3_FQDN ]; then
         read -r -p "Are you sure you want to overwrite $LAYER_NAME_38? [y/N] " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
         then
-            aws s3 cp lambda_layer-python3.8.zip s3://$LAMBDA_LAYERS_BUCKET
-            aws lambda publish-layer-version --layer-name $LAYER_NAME_38 --content S3Bucket=$LAMBDA_LAYERS_BUCKET,S3Key=lambda_layer-python3.8.zip --compatible-runtimes python3.8
-            aws s3 rm s3://$LAMBDA_LAYERS_BUCKET/lambda_layer-python3.8.zip
+            aws s3 cp lambda_layer-python3.8.zip s3://"$LAMBDA_LAYERS_BUCKET"
+            aws lambda publish-layer-version --layer-name $LAYER_NAME_38 --content S3Bucket="$LAMBDA_LAYERS_BUCKET",S3Key=lambda_layer-python3.8.zip --compatible-runtimes python3.8
+            aws s3 rm s3://"$LAMBDA_LAYERS_BUCKET"/lambda_layer-python3.8.zip
             arn38=$(aws lambda list-layer-versions --layer-name lambda_layer-python37 --output text --query 'LayerVersions[0].LayerVersionArn')
         fi
     fi
     # remove temp working dir for zip files
-    aws s3 rb s3://$LAMBDA_LAYERS_BUCKET/ > /dev/null    
+    aws s3 rb s3://"$LAMBDA_LAYERS_BUCKET"/ > /dev/null
     echo "Lambda layers have been published. Use the following ARNs to attach them to Lambda functions:"
-    echo $arn36
-    echo $arn37
-    echo $arn38
+    echo "$arn36"
+    echo "$arn37"
+    echo "$arn38"
 fi
 
 echo "------------------------------------------------------------------------------"

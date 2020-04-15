420275f7d21c3c7e8ff74b74f273395ad3b19f5b
ianwow <ianwow@amazon.com>
2020-04-14 22:09:30 -0700

    implement add and delete buttons for captions

--- source/webapp/src/components/Transcript.vue
+++ source/webapp/src/components/Transcript.vue
@@ -36,14 +36,18 @@
           <b-container class="p-0">
             <b-row no-gutters>
               <b-col cols="10">
-          <b-form-textarea class="custom-text-field .form-control-sm" max-rows="8" v-model="data.item.caption"/>
+          <b-form-textarea :ref="'caption' + data.index" class="custom-text-field .form-control-sm" max-rows="8" v-model="data.item.caption" placeholder="Type subtitle here"/>
               </b-col>
               <b-col>
-                <span style="position:absolute; top: 3px">
-                  <b-icon-x-circle color="lightgrey"></b-icon-x-circle><br><br>
+                <span style="position:absolute; top: 0px">
+                  <b-button size="sm" variant="link" @click="delete_row(data.index)">
+                    <b-icon icon="x-circle" color="lightgrey"></b-icon>
+                  </b-button>
                 </span>
                 <span style="position:absolute; bottom: 0px">
-                  <b-icon-plus-square color="lightgrey"></b-icon-plus-square>
+                  <b-button size="sm" variant="link" @click="add_row(data.index)">
+                    <b-icon icon="plus-square" color="lightgrey"></b-icon>
+                  </b-button>
                 </span>
               </b-col>
             </b-row>
@@ -51,6 +55,20 @@
         </template>
       </b-table>
       </div>
+    </div>
+    <div>
+      <b-dropdown dropup id="dropdown-1" text="Actions" class="m-md-2">
+<!--        <b-dropdown-item @click="showModal()">Upload transcript</b-dropdown-item>-->
+        <b-dropdown-item>Download transcript</b-dropdown-item>
+        <b-dropdown-item>Save changes</b-dropdown-item>
+      </b-dropdown>
+
+<!--      <b-modal ref="my-modal" hide-footer title="Upload a file">-->
+<!--        <p>Upload a timed subtitles file in the Webcaptions JSON format.</p>-->
+<!--        <div>-->
+<!--          <input type="file" @change="previewFiles">-->
+<!--        </div>-->
+<!--      </b-modal>-->
     </div>
   </div>
 </template>
@@ -64,6 +82,8 @@ export default {
     return {
       webCaptions: webcaptions_file,
       webCaptions_fields: ['timeslot', 'caption'],
+      uploaded_captions_file: '',
+      id: 0,
       transcript: "",
       isBusy: false,
       operator: "transcript",
@@ -88,6 +108,23 @@ export default {
       this.transcript = ''
   },
   methods: {
+    // showModal() {
+    //   this.$refs['my-modal'].show()
+    // },
+    // previewFiles(event) {
+    //   console.log(event.target.files);
+    //   const file = event.target.files[0];
+    //   const reader = new FileReader();
+    //   reader.onload = e => this.$emit("load", e.target.result);
+    //   reader.readAsText(file);
+    // },
+    add_row(index) {
+      this.webCaptions.splice(index+1, 0, {"start":this.webCaptions[index].end,"caption":"","end":this.webCaptions[index+1].start})
+      this.$refs["caption"+(index+1)].focus();
+    },
+    delete_row(index) {
+      this.webCaptions.splice(index, 1)
+    },
     async fetchAssetData () {
       let query = 'AssetId:'+this.$route.params.asset_id+ ' _index:mietranscript';
       let apiName = 'mieElasticsearch';
@@ -141,6 +178,17 @@ export default {
     height: 500px;
     border-top: 1px solid #e2e2e2;
     border-bottom: 1px solid #e2e2e2;
+  }
+  /* these options needed for MacOS to make scrollbar visible when not in use */
+  .event-line-editor::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 7px;
+  }
+  /* these options needed for MacOS to make scrollbar visible when not in use */
+  .event-line-editor::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    background-color: rgba(0, 0, 0, .5);
+    -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, .5);
   }
   .custom-text-field {
     border: 0;

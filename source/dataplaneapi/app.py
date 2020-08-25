3d711508fda3e60f27a5a7f55e7f31ad47be4f01
ianwow <ianwow@amazon.com>
2020-08-24 23:00:48 -0700

    fix datetime bug

--- source/dataplaneapi/app.py
+++ source/dataplaneapi/app.py
@@ -677,8 +677,6 @@ def create_vocabulary():
         LanguageCode=language_code,
         VocabularyFileUri=json.loads(app.current_request.raw_body.decode())['s3uri']
     )
-    response['TerminologyProperties']['CreatedAt'] = response['TerminologyProperties']['CreatedAt'].isoformat()
-    response['TerminologyProperties']['LastUpdatedAt'] = response['TerminologyProperties']['LastUpdatedAt'].isoformat()
     return response
 
 

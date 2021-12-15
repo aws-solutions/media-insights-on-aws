d1f65083d0bbae5efbe0852577791122f2a61282
ianwow <ianwow@amazon.com>
2021-12-15 08:24:37 -0800

    upgrade MIE lambda layer from 0.0.4 to 1.0.0

--- source/lib/MediaInsightsEngineLambdaHelper/setup.py
+++ source/lib/MediaInsightsEngineLambdaHelper/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Media Insights Engine Lambda Helper",
-    version="0.0.4",
+    version="1.0.0",
     author="Brandon Dold",
     author_email="redacted@example.com",
     description="Helper classes for developing Media Insights Engine Lambda Functions",
